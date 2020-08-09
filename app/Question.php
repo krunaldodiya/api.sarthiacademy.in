14bfe0b1e6da061431a488f416e13820baed0650
kunal dodiya <kunal.dodiya1@gmail.com>
2020-08-09 15:00:26 +0530

    done

--- app/Question.php
+++ app/Question.php
@@ -15,4 +15,14 @@ class Question extends Model
     protected $dates = [
         'created_at', 'updated_at',
     ];
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
+    public function chapter()
+    {
+        return $this->belongsTo(Chapter::class);
+    }
 }
