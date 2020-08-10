e52ffa915f1be8b54306e22054b0d29b2ad138be
kunal dodiya <kunal.dodiya1@gmail.com>
2020-08-10 07:24:24 +0530

    done

--- app/Attachment.php
+++ app/Attachment.php
@@ -2,9 +2,22 @@
 
 namespace App;
 
+use Jamesh\Uuid\HasUuid;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Attachment extends Model
 {
-    //
+    use HasUuid;
+
+    protected $guarded = [];
+
+    protected $dates = [
+        'created_at', 'updated_at',
+    ];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
 }
--- app/Test.php
+++ app/Test.php
@@ -2,9 +2,22 @@
 
 namespace App;
 
+use Jamesh\Uuid\HasUuid;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Test extends Model
 {
-    //
+    use HasUuid;
+
+    protected $guarded = [];
+
+    protected $dates = [
+        'created_at', 'updated_at',
+    ];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
 }
