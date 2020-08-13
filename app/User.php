d7dbb7cbb611f561b0c944dd20a7cee4d62cee6e
kunal dodiya <kunal.dodiya1@gmail.com>
2020-08-13 11:11:28 +0530

    done

--- app/User.php
+++ app/User.php
@@ -7,10 +7,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 use Jamesh\Uuid\HasUuid;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable, HasUuid;
+    use Notifiable, HasApiTokens, HasUuid;
 
     protected $guarded = [];
 
