9f36dff57a4a908bcc857c1bd3680a88a047039a
kunal dodiya <kunal.dodiya1@gmail.com>
2020-08-14 16:51:25 +0530

    added country_id validation

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -69,7 +69,8 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
                  "kunal.dodiya1@gmail.com",
-                 "aryanadya@gmail.com"
+                 "aryanadya@gmail.com",
+                 "admin@sarthiacademy.in"
              ]);
         });
     }
