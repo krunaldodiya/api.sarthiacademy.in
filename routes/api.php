03b2fc3ef839baf95880e14527916f7847ab8f13
kunal dodiya <kunal.dodiya1@gmail.com>
2020-08-16 10:13:41 +0530

    done

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,8 @@ Route::middleware('auth:sanctum')->post('/feedback/send', 'FeedbackController@se
 
 Route::middleware('auth:sanctum')->get('/courses/all', 'CourseController@getCourses');
 Route::middleware('auth:sanctum')->get('/courses/materials', 'CourseController@getCourseMaterials');
-Route::middleware('auth:sanctum')->get('/courses/tests', 'CourseController@getCourseMaterials');
-Route::middleware('auth:sanctum')->get('/courses/attachments', 'CourseController@getCourseMaterials');
+Route::middleware('auth:sanctum')->get('/courses/tests', 'CourseController@getCourseTests');
+Route::middleware('auth:sanctum')->get('/courses/attachments', 'CourseController@getCourseAttachments');
 
 Route::middleware('auth:sanctum')->get('/notifications/all', 'NotificationController@getNotifications');
 Route::middleware('auth:sanctum')->post('/notifications/read', 'NotificationController@markAsRead');
