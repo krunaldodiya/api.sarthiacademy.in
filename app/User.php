6329215fea8940939d5c25d7e42f9521de31f99e
kunal dodiya <kunal.dodiya1@gmail.com>
2020-08-18 16:38:27 +0530

    done

--- app/User.php
+++ app/User.php
@@ -37,7 +37,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    protected $appends = ['setting', 'avatar'];
+    protected $appends = ['setting'];
 
     public function getAvatarAttribute()
     {
