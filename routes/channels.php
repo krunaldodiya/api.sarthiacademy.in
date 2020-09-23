cdacb8a8679875522c64e1da5221efdc59a7b5c7
Krunal Dodiya <kunal.dodiya1@gmail.com>
2020-09-23 09:32:57 +0530

    added size to video quality

--- routes/channels.php
+++ routes/channels.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('App.User.{id}', function ($user, $id) {
+Broadcast::middleware('auth:sanctum')->channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
