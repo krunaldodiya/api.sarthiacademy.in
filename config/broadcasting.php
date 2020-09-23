a4df1d405ef76f62b50723f7c3d5995c6a13a6ee
Krunal Dodiya <kunal.dodiya1@gmail.com>
2020-09-23 14:34:29 +0530

    added size to video quality

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,7 +36,6 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'encrypted' => false,
                 'cluster'   =>  env('PUSHER_APP_CLUSTER'),
                 'host' => env('PUSHER_APP_HOST'),
                 'port' => env('PUSHER_APP_PORT'),
