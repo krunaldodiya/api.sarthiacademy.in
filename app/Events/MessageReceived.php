ea291a1954cb474b28487cf361c2e93b13b93d26
Krunal Dodiya <kunal.dodiya1@gmail.com>
2020-09-23 16:43:30 +0530

    added chat

--- app/Events/MessageReceived.php
+++ app/Events/MessageReceived.php
@@ -10,6 +10,8 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
+use App\Chat;
+
 class MessageReceived implements ShouldBroadcast
 {
     public $chat;
