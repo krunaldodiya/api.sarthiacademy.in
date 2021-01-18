023accd55542048b308bf5c5cc6feb545c1ec3d9
krunal dodiya <kunal.dodiya1@gmail.com>
2021-01-18 17:35:52 +0530

    added improved

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string
      */
-    protected $proxies;
+    protected $proxies = ['10.0.1.98'];
 
     /**
      * The headers that should be used to detect proxies.
